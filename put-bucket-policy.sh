07797e19cc458f409e4b4ce8d754dd1942d01db8
Ubuntu <ubuntu@ip-172-31-36-91.ap-south-1.compute.internal>
2019-08-03 15:09:29 +0000

    added jenkins/mediawiki

---
+++ put-bucket-policy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DEFAULT_REGION="ap-south-1"
+AWS_REGION="${AWS_REGION:-${DEFAULT_REGION}}"
+
+BUCKET="helm-kebko0gq"
+NODE_ROLE_ARN="arn:aws:iam::828054316136:role/masters.vinga.cf"
+
+export AWS_REGION
+
+aws s3api put-bucket-policy --bucket ${BUCKET} --region $AWS_REGION --policy '{
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "AWS": "'${NODE_ROLE_ARN}'"
+          },
+         "Action": [
+          "s3:GetObject",
+          "s3:PutObject"
+          ],
+         "Resource": "arn:aws:s3:::'${BUCKET}'/*"
+      },
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "AWS": "'${NODE_ROLE_ARN}'"
+          },
+         "Action": "s3:ListBucket",
+         "Resource": "arn:aws:s3:::'${BUCKET}'"
+      }
+  ]
+}'
